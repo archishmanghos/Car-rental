648a84d9999bcc48680848bd40a4c5d4e12df8b7
Archishman <ag2324@it.jgec.ac.in>
2023-10-30 18:06:58 +0530

    Completed login/register popup

--- Frontend/src/app/myComponents/login-register/login-register.component.ts
+++ Frontend/src/app/myComponents/login-register/login-register.component.ts
@@ -12,4 +12,21 @@ export class LoginRegisterComponent {
   onClosePopup() {
     this.closePopup.emit();
   }
+
+  ngAfterViewInit() {
+    (document.querySelector('#myTab') as HTMLElement).addEventListener('click', (event) => {
+      event.preventDefault();
+      const tab = event.target as HTMLElement;
+      if (tab.getAttribute('data-toggle') === 'tab') {
+        const target = tab.getAttribute('href');
+        
+        if (target) {
+          (document.querySelector('.tab-pane.show') as HTMLElement).classList.remove('show');
+          (document.querySelector('.tab-pane.active') as HTMLElement).classList.remove('active');
+          (document.querySelector(tab.getAttribute('href') as string) as HTMLElement).classList.add('active');
+          (document.querySelector(tab.getAttribute('href') as string) as HTMLElement).classList.add('show');
+        }
+      }
+    });
+  }
 }

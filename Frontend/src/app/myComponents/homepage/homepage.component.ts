4ebf567214d616c4631546226752fceabd3fa45f
Archishman <ag2324@it.jgec.ac.in>
2023-11-07 14:39:06 +0530

    Add testimonial section

--- Frontend/src/app/myComponents/homepage/homepage.component.ts
+++ Frontend/src/app/myComponents/homepage/homepage.component.ts
@@ -19,4 +19,25 @@ export class HomepageComponent {
   toRent() {
     document.getElementById("rent")!.scrollIntoView();
   }
+
+  testimonials: any[] = [
+    {
+      name: "Anju",
+      social: ["mangomanga", "mangomanga", "anjuCooks"],
+      image: "testimonial/person1.jpg",
+      quote: "ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis quae aperiam veritatis eius voluptate excepturi a. Enim quaerat exercitationem natus distinctio? Quibusdam amet laudantium ratione quia provident magnam inventore voluptatibus pariatur nemo harum sit fuga laboriosam ipsum iste, aut autem."
+    },
+    {
+      name: "Baipool",
+      social: ["lovesbi", "lovesbi", "baipoolLoves"],
+      image: "testimonial/person2.avif",
+      quote: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nostrum praesentium molestias esse quas eos voluptatum dolore voluptas accusamus temporibus qui quidem, dolores tenetur nulla fuga suscipit repudiandae et laboriosam enim. Aliquid sed soluta est natus? Ab deserunt unde saepe ratione."
+    },
+    {
+      name: "beANandi",
+      social: ["loveTalibani", "loveTalibani", "beANandi"],
+      image: "testimonial/person3.avif",
+      quote: "Lorem ipsum dolor sit amet consectetur adipisicing elit. At hic animi distinctio quidem eveniet odit dolorem placeat nostrum vel in eligendi exercitationem porro iste dolor, minima veritatis cupiditate soluta tenetur impedit possimus reprehenderit dignissimos molestiae! Sit quae tenetur inventore eum!"
+    }
+  ]
 }
--- Frontend/src/app/myComponents/testimonials/testimonials.component.ts
+++ Frontend/src/app/myComponents/testimonials/testimonials.component.ts
@@ -1,11 +1,35 @@
-import { Component } from '@angular/core';
-import { faQuoteLeft } from '@fortawesome/free-solid-svg-icons';
+import { Component, Input, OnInit } from '@angular/core';
+import { faQuoteLeft, faQuoteRight } from '@fortawesome/free-solid-svg-icons';
+import { faTwitter, faYoutube, faInstagram } from '@fortawesome/free-brands-svg-icons';
+import Swiper from 'swiper';
+// import SwiperC
 
 @Component({
   selector: 'app-testimonials',
   templateUrl: './testimonials.component.html',
-  styleUrls: ['./testimonials.component.css']
+  styleUrls: ['./testimonials.component.css'],
 })
-export class TestimonialsComponent {
+export class TestimonialsComponent implements OnInit {
     faQuoteLeft = faQuoteLeft;
+    faQuoteRight = faQuoteRight;
+    faTwitter = faTwitter;
+    faInstagram = faInstagram;
+    faYoutube = faYoutube;
+
+    @Input() testimonials: any[] = [];
+
+    swiper: Swiper | undefined;
+
+    ngOnInit(): void {
+      this.swiper=new Swiper('.swiper-container', {
+            slidesPerView: 1,
+            spaceBetween: 30,
+            navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+      });
+    }
+
+
 }
